1738f45d4e9c8b9c68dc57420de8e0c77b01bd66
Ricks <pricardo03@users.noreply.github.com>
2015-02-04 23:13:33 -0600

    Create plot1.R

---
+++ Project1/plot1.R
@@ -0,0 +1,22 @@
+library(datasets)
+
+#Reading all data
+PowerConsumption <- read.csv("household_power_consumption.txt", header = T, sep=";", na.string="?", ## nrows=2075259,
+                              check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+
+#set format date to get a clean filtered data
+PowerConsumption$Date <- as.Date(PowerConsumption$Date, format="%d/%m/%Y")
+
+#get only two days
+DataPeriod <- subset(PowerConsumption, subset(Date>= "2007-02-01" & Date<="2007-02-02"))
+#deallocating resources
+rm(PowerConsumption)
+
+#Create png file
+png(file="plot1.png")
+
+#Show de plot
+hist(DataPeriod$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power(kilowatts)", ylab="Frecuency")
+
+#Close device
+png(file="plot1.png")
