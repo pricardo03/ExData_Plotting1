0e9e292e31134d611511fd1877c227ca2da88ce8
Ricks <pricardo03@users.noreply.github.com>
2015-02-11 19:40:08 -0600

    Update plot3.R

--- Project1/plot3.R
+++ Project1/plot3.R
@@ -27,5 +27,5 @@ legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"
 
 
 #save into a png file from screen
-dev.copy(png, file="plot3.png", height=480, width=480)
+dev.copy(png, file="plot3.png", height=480, width=480, units = "px", bg = "transparent")
 dev.off()
